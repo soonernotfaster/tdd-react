9d7ad4fc5e14d2d0e0d7ceeaee1a473f7412405a
Steven Solomon <solomon.steven.m@gmail.com>
2025-04-27 20:26:53 +0000

    Speed up baseline test config

--- bowling-kata/vitest.config.ts
+++ bowling-kata/vitest.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         globals: true,
-        environment: 'jsdom',
+        environment: 'happy-dom',
+        coverage: {
+            enabled: false
+        },
+        sequence: {
+            shuffle: true,
+            concurrent: true
+        },
     },
 })

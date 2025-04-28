c72924b88b74b10702a16342ee0f2f4aee45c088
Steven Solomon <solomon.steven.m@gmail.com>
2025-04-28 15:37:41 +0000

    Add git aliases

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 npm install --prefix bowling-kata
+
+# Setup git aliases
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status

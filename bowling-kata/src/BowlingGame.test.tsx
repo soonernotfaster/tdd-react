5270711e44718df0422a177cf51f1c6bcdcff230
Steven Solomon <solomon.steven.m@gmail.com>
2025-04-28 15:39:52 +0000

    Extract custom mathcer

--- bowling-kata/src/BowlingGame.test.tsx
+++ bowling-kata/src/BowlingGame.test.tsx
@@ -6,15 +6,12 @@ describe(BowlingGame, () => {
     it("has 10 empty frames", async () => {
         render(<BowlingGame />)
 
-        expect(screen.getByTestId("frame1-score")).toBeTruthy()
-        expect(screen.getByTestId("frame2-score")).toBeTruthy()
-        expect(screen.getByTestId("frame3-score")).toBeTruthy()
-        expect(screen.getByTestId("frame4-score")).toBeTruthy()
-        expect(screen.getByTestId("frame5-score")).toBeTruthy()
-        expect(screen.getByTestId("frame6-score")).toBeTruthy()
-        expect(screen.getByTestId("frame7-score")).toBeTruthy()
-        expect(screen.getByTestId("frame8-score")).toBeTruthy()
-        expect(screen.getByTestId("frame9-score")).toBeTruthy()
-        expect(screen.getByTestId("frame10-score")).toBeTruthy()
+        for (let frame = 1; frame < 11; frame++)
+            expectScoreToBe(frame, "")
+        
     })
+
+    function expectScoreToBe(frame: number, score: string) {
+        expect(screen.getByTestId(`frame${frame}-score`).innerText).toEqual(score)
+    }
 })

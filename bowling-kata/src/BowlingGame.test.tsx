976e30ad030df413335b1e2320be1a6c70ccbcc7
Steven Solomon <solomon.steven.m@gmail.com>
2025-04-29 17:01:03 +0000

    Extract table test

--- bowling-kata/src/BowlingGame.test.tsx
+++ bowling-kata/src/BowlingGame.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it, beforeEach, expect } from "vitest"
+import { describe, it, beforeEach, expect, vitest } from "vitest"
 import { render, screen, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import BowlingGame from "./BowlingGame"
@@ -25,60 +25,34 @@ describe(BowlingGame, () => {
         })
     })
 
-    describe("when all gutters rolled", () => {
-        beforeEach(() => {
-            userEvent.setup()
-
-            render(<BowlingGame />)
-
-            for (let gutters = 0; gutters < 20; gutters++)
-                userEvent.click(screen.getByTestId("gutter"))
-        })
-
-        it("has zero for each roll", async () => {
-            await waitFor(() => {
-                for (let frame = 1; frame < 11; frame++) {
-                    expectRollToBe(frame, 1, "0")
-                    expectRollToBe(frame, 2, "0")
-                }
-            })
-        })
-
-        it("has score of zero", async () => {
-            await waitFor(() => {
-                for (let frame = 1; frame < 11; frame++)
-                    expectScoreToBe(frame, "0")
-
-                expect(screen.getByTestId("total-score").innerText).toEqual("0")
-            })
-        })
-    })
-
-    describe("when all 1s are rolled", () => {
+    describe.each([
+        {rollButton: "1-pin", rollScore: "1", frameScore: "2", totalScore: "20"},
+        {rollButton: "gutter", rollScore: "0", frameScore: "0", totalScore: "0"},
+    ])("when each roll is $rollButton", ({rollButton, rollScore, frameScore, totalScore}) => {
         beforeEach(() => {
             userEvent.setup()
 
             render(<BowlingGame />)
 
             for (let rolls = 0; rolls < 20; rolls++)
-                userEvent.click(screen.getByTestId("1-pin"))
+                userEvent.click(screen.getByTestId(rollButton))
         })
 
-        it("has 1 pin for each roll", async () => {
+        it(`has ${rollButton} for each roll`, async () => {
             await waitFor(() => {
                 for (let frame = 1; frame < 11; frame++) {
-                    expectRollToBe(frame, 1, "1")
-                    expectRollToBe(frame, 2, "1")
+                    expectRollToBe(frame, 1, rollScore)
+                    expectRollToBe(frame, 2, rollScore)
                 }
             })
         })
 
-        it("has score of 20", async () => {
+        it(`has score of ${totalScore}`, async () => {
             await waitFor(() => {
                 for (let frame = 1; frame < 11; frame++)
-                    expectScoreToBe(frame, "2")
+                    expectScoreToBe(frame, frameScore)
 
-                expect(screen.getByTestId("total-score").innerText).toEqual("20")
+                expect(screen.getByTestId("total-score").innerText).toEqual(totalScore)
             })
         })
     })

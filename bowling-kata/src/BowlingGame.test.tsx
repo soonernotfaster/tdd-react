9f5d5db542cd09323cf7ec761b9236f7dd55c221
Steven Solomon <solomon.steven.m@gmail.com>
2025-04-29 17:34:02 +0000

    Format code

--- bowling-kata/src/BowlingGame.test.tsx
+++ bowling-kata/src/BowlingGame.test.tsx
@@ -1,96 +1,100 @@
-import { describe, it, beforeEach, expect, vitest } from "vitest"
-import { render, screen, waitFor } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
-import BowlingGame from "./BowlingGame"
+import { describe, it, beforeEach, expect, vitest } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import BowlingGame from "./BowlingGame";
 
 describe(BowlingGame, () => {
-    describe("initial render", () => {
-        it("has 10 empty frames", async () => {
-            render(<BowlingGame />)
+  describe("initial render", () => {
+    it("has 10 empty frames", async () => {
+      render(<BowlingGame />);
 
-            for (let frame = 1; frame < 11; frame++)
-                expectFrameScoreToBe(frame, "")
-        })
+      for (let frame = 1; frame < 11; frame++) expectFrameScoreToBe(frame, "");
+    });
 
-        it("has no rolls for any frames", () => {
-            render(<BowlingGame />)
+    it("has no rolls for any frames", () => {
+      render(<BowlingGame />);
 
-            expect(screen.queryAllByTestId("roll")).toHaveLength(0)
-        })
+      expect(screen.queryAllByTestId("roll")).toHaveLength(0);
+    });
 
-        it("does not show score", () => {
-            render(<BowlingGame />)
+    it("does not show score", () => {
+      render(<BowlingGame />);
 
-            expect(screen.getByTestId("total-score").innerText).toEqual("")
-        })
-    })
+      expect(screen.getByTestId("total-score").innerText).toEqual("");
+    });
+  });
 
-    describe.each([
-        {rollButton: "gutter", rollScore: "0", frameScore: "0", totalScore: "0"},
-        {rollButton: "1-pin", rollScore: "1", frameScore: "2", totalScore: "20"},
-        {rollButton: "2-pin", rollScore: "2", frameScore: "4", totalScore: "40"},
-        {rollButton: "3-pin", rollScore: "3", frameScore: "6", totalScore: "60"},
-        {rollButton: "4-pin", rollScore: "4", frameScore: "8", totalScore: "80"},
-    ])("when each roll is $rollButton", ({rollButton, rollScore, frameScore, totalScore}) => {
-        beforeEach(() => {
-            userEvent.setup()
+  describe.each([
+    { rollButton: "gutter", rollScore: "0", frameScore: "0", totalScore: "0" },
+    { rollButton: "1-pin", rollScore: "1", frameScore: "2", totalScore: "20" },
+    { rollButton: "2-pin", rollScore: "2", frameScore: "4", totalScore: "40" },
+    { rollButton: "3-pin", rollScore: "3", frameScore: "6", totalScore: "60" },
+    { rollButton: "4-pin", rollScore: "4", frameScore: "8", totalScore: "80" },
+  ])(
+    "when each roll is $rollButton",
+    ({ rollButton, rollScore, frameScore, totalScore }) => {
+      beforeEach(() => {
+        userEvent.setup();
 
-            render(<BowlingGame />)
+        render(<BowlingGame />);
 
-            for (let rolls = 0; rolls < 20; rolls++)
-                userEvent.click(screen.getByTestId(rollButton))
-        })
+        for (let rolls = 0; rolls < 20; rolls++)
+          userEvent.click(screen.getByTestId(rollButton));
+      });
 
-        it(`has ${rollButton} for each roll`, async () => {
-            await waitFor(() => {
-                for (let frame = 1; frame < 11; frame++) {
-                    expectRollToBe(frame, 1, rollScore)
-                    expectRollToBe(frame, 2, rollScore)
-                }
+      it(`has ${rollButton} for each roll`, async () => {
+        await waitFor(() => {
+          for (let frame = 1; frame < 11; frame++) {
+            expectRollToBe(frame, 1, rollScore);
+            expectRollToBe(frame, 2, rollScore);
+          }
 
-                for (let frame = 1; frame < 11; frame++)
-                    expectFrameScoreToBe(frame, frameScore)
-            })
-        })
+          for (let frame = 1; frame < 11; frame++)
+            expectFrameScoreToBe(frame, frameScore);
+        });
+      });
 
-        it(`has score of ${totalScore}`, async () => {
-            await waitFor(() => {
-                expect(screen.getByTestId("total-score").innerText).toEqual(totalScore)
-            })
-        })
-    })
-
-    describe("when first frame is spare", () => {
-        beforeEach(() => {
-            userEvent.setup()
-    
-            render(<BowlingGame />)
-    
-            userEvent.click(screen.getByTestId("5-pin"))
-            userEvent.click(screen.getByTestId("5-pin"))
-        })
-
-        describe("all gutters", () => {
-            beforeEach(() => {
-                for (let rolls = 0; rolls < 18; rolls++)
-                    userEvent.click(screen.getByTestId("gutter"))    
-            })
-        
-            it("has 10 has total", async () => {
-                await waitFor(() => {
-                    expect(screen.getByTestId("total-score").innerText).toEqual("10")
-                })
-            })    
-        })
-    })
-
-    function expectRollToBe(frame: number, roll: number, expectedPins: string) {
-        const rollId = `frame${frame}-roll${roll}`
-        expect(screen.getByTestId(rollId).innerText, rollId).toEqual(expectedPins)
+      it(`has score of ${totalScore}`, async () => {
+        await waitFor(() => {
+          expect(screen.getByTestId("total-score").innerText).toEqual(
+            totalScore
+          );
+        });
+      });
     }
+  );
 
-    function expectFrameScoreToBe(frame: number, score: string) {
-        const frame_id = `frame${frame}-score`
-        expect(screen.getByTestId(frame_id).innerText, frame_id).toEqual(score)
-    }
-})
+  describe("when first frame is spare", () => {
+    beforeEach(() => {
+      userEvent.setup();
+
+      render(<BowlingGame />);
+
+      userEvent.click(screen.getByTestId("5-pin"));
+      userEvent.click(screen.getByTestId("5-pin"));
+    });
+
+    describe("all gutters", () => {
+      beforeEach(() => {
+        for (let rolls = 0; rolls < 18; rolls++)
+          userEvent.click(screen.getByTestId("gutter"));
+      });
+
+      it("has 10 as total", async () => {
+        await waitFor(() => {
+          expect(screen.getByTestId("total-score").innerText).toEqual("10");
+        });
+      });
+    });
+  });
+
+  function expectRollToBe(frame: number, roll: number, expectedPins: string) {
+    const rollId = `frame${frame}-roll${roll}`;
+    expect(screen.getByTestId(rollId).innerText, rollId).toEqual(expectedPins);
+  }
+
+  function expectFrameScoreToBe(frame: number, score: string) {
+    const frame_id = `frame${frame}-score`;
+    expect(screen.getByTestId(frame_id).innerText, frame_id).toEqual(score);
+  }
+});
--- bowling-kata/src/BowlingGame.tsx
+++ bowling-kata/src/BowlingGame.tsx
@@ -1,65 +1,83 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function frameTotal(rolls: number[]): string {
-    if (rolls.length === 0)
-        return ""
-    else
-        return `${(rolls[0] || 0) + (rolls[1] || 0)}`
+  if (rolls.length === 0) return "";
+  else return `${(rolls[0] || 0) + (rolls[1] || 0)}`;
 }
 
 function totalScore(frames: number[][]): string {
-    if (frames.length < 10)
-        return ""
-    const total = frames.reduce((prev, curr) => prev + ((curr[0] || 0) + (curr[1] || 0)), 0)
-    return `${total}`
+  if (frames.length < 10) return "";
+  const total = frames.reduce(
+    (prev, curr) => prev + ((curr[0] || 0) + (curr[1] || 0)),
+    0
+  );
+  return `${total}`;
 }
 
 function BowlingGame() {
-    const [currentFrame, setCurrentFrame] = useState<number>(0)
+  const [currentFrame, setCurrentFrame] = useState<number>(0);
 
-    const [rolls, setRolls] = useState<number[][]>(Array.from({ length: 10 }).fill([]) as number[][])
+  const [rolls, setRolls] = useState<number[][]>(
+    Array.from({ length: 10 }).fill([]) as number[][]
+  );
 
-    const handleRoll = (numPins: number) => {
-        return () => {
-            let frameFinished = false
-            const updatedRolls = rolls.map((frameRolls, frameIndex) => {
-                if (frameIndex === currentFrame) {
-                    if (frameRolls.length === 1)
-                        frameFinished = true
-                    return [...frameRolls, numPins]
-                }
-                return frameRolls
-            })
-            setRolls(updatedRolls)
-            if (frameFinished)
-                setCurrentFrame(currentFrame + 1)
+  const handleRoll = (numPins: number) => {
+    return () => {
+      let frameFinished = false;
+      const updatedRolls = rolls.map((frameRolls, frameIndex) => {
+        if (frameIndex === currentFrame) {
+          if (frameRolls.length === 1) frameFinished = true;
+          return [...frameRolls, numPins];
         }
-    }
-    return <div>
-        {rolls.map((frameRolls, index) => {
+        return frameRolls;
+      });
+      setRolls(updatedRolls);
+      if (frameFinished) setCurrentFrame(currentFrame + 1);
+    };
+  };
+  return (
+    <div>
+      {rolls.map((frameRolls, index) => {
+        return (
+          <div key={index}>
+            <div data-testid={`frame${index + 1}-score`}>
+              {frameTotal(frameRolls)}
+            </div>
+            <div data-testid={`frame${index + 1}-roll${1}`}>
+              {frameRolls[0]}
+            </div>
+            <div data-testid={`frame${index + 1}-roll${2}`}>
+              {frameRolls[1]}
+            </div>
+          </div>
+        );
+      })}
 
-            return (
-                <div key={index}>
-                    <div data-testid={`frame${index + 1}-score`}>
-                        {frameTotal(frameRolls)}
-                    </div>
-                    <div data-testid={`frame${index + 1}-roll${1}`}>{frameRolls[0]}</div>
-                    <div data-testid={`frame${index + 1}-roll${2}`}>{frameRolls[1]}</div>
-                </div>
-            )
-        })}
-
-
-        <div data-testid="total-score">{currentFrame > 9 && totalScore(rolls)}</div>
-        <div>
-            <button data-testid="gutter" onClick={handleRoll(0)}>Gutter Ball</button>
-            <button data-testid="1-pin" onClick={handleRoll(1)}>1 pin</button>
-            <button data-testid="2-pin" onClick={handleRoll(2)}>2 pin</button>
-            <button data-testid="3-pin" onClick={handleRoll(3)}>3 pin</button>
-            <button data-testid="4-pin" onClick={handleRoll(4)}>4 pin</button>
-            <button data-testid="5-pin" onClick={handleRoll(5)}>4 pin</button>
-        </div>
+      <div data-testid="total-score">
+        {currentFrame > 9 && totalScore(rolls)}
+      </div>
+      <div>
+        <button data-testid="gutter" onClick={handleRoll(0)}>
+          Gutter Ball
+        </button>
+        <button data-testid="1-pin" onClick={handleRoll(1)}>
+          1 pin
+        </button>
+        <button data-testid="2-pin" onClick={handleRoll(2)}>
+          2 pin
+        </button>
+        <button data-testid="3-pin" onClick={handleRoll(3)}>
+          3 pin
+        </button>
+        <button data-testid="4-pin" onClick={handleRoll(4)}>
+          4 pin
+        </button>
+        <button data-testid="5-pin" onClick={handleRoll(5)}>
+          4 pin
+        </button>
+      </div>
     </div>
+  );
 }
 
-export default BowlingGame
+export default BowlingGame;
